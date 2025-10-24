76ba3f23fb018ec76b4608fb5e6444a4e3a8bc36
Lars Uffmann <lars.uffmann@gmail.com>
2025-10-24 12:24:47 +0200

    first commit

---
+++ src/main/java/org/cachescrubber/demo/demo_actuator_jackson2/DemoActuatorJackson2Application.java
@@ -0,0 +1,13 @@
+package org.cachescrubber.demo.demo_actuator_jackson2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoActuatorJackson2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoActuatorJackson2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/cachescrubber/demo/demo_actuator_jackson2/DemoActuatorJackson2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cachescrubber.demo.demo_actuator_jackson2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoActuatorJackson2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
